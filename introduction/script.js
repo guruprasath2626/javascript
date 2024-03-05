cd4a335a55d5a9becbea73b766439b1cf4c78e27
guruprasath26 <karthikguruprasath2626@gmail.com>
2024-03-05 19:13:15 +0530

    first commit

---
+++ introduction/script.js
@@ -0,0 +1,45 @@
+//single line comments
+        /*multi ine comment*/
+        // console.log("hapy")
+        // alert("hai")
+
+        //variables
+/*
+        let score=10 //initialisation
+        score=20 //assingment
+
+        console.log("score is" + score);
+
+        const pi =3.14
+        console.log(pi)
+        var a=30
+        a=40
+        console.log(a)
+
+*/
+/*
+operators
+---------
+
+
+Arthmetric operators: +  -  * **  / %  ++  --
+Assignment operators: =  +=  -=  *=  /=  %=  **=
+Comparison operators: ==  ===  !=  !==  >  <  >=  <=  
+Logical operators: &&  ||  !
+Bitwise operators: &  |  ~  ^  <<  >>  >>>
+
+
+
+*/
+
+/*
+javascript has a autoconversion 
+a="5"-1
+console.log(a);
+o/p=4
+*/
+
+
+a=10
+b=30
+console.log(a>b)
